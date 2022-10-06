5757f323bb06d55b7789908a0b19783054c4fa8c
omarageorge <georgeomara8@gmail.com>
2022-10-06 12:02:29 +0300

    Another commit

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -5,14 +5,6 @@ import Product from '../components/Product';
 export default function Products() {
   return (
     <AdminLayout>
-      {/* <div className='text-right pt-8 pr-4'>
-          <Link to='#'>
-            <a className='bg-lime-900 hover:bg-lime-800 py-4 px-8 rounded-sm font-light text-lg text-gray-100 first-letter transition-all delay-100 ease-out'>
-              Add New
-            </a>
-          </Link>
-        </div> */}
-
       <div className='w-full h-auto grid grid-cols-1 gap-6 px-6 py-6 sm:grid-cols-2 md:grid-cols-3'>
         <Product />
         <Product />
