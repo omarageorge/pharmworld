69ae26e1b2dd36ee90d2acc25713a4e137684d70
omarageorge <georgeomara8@gmail.com>
2022-10-08 14:46:55 +0300

    Another commit

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,14 @@
 import Image from 'next/image';
+import { useState } from 'react';
 import { FaCartPlus } from 'react-icons/fa';
 
-export default function ProductCard({ details }) {
+export default function ProductCard({ product, price, image }) {
+  const [added, setAdded] = useState(false);
+
   return (
     <div className='block overflow-hidden rounded-md bg-lime-400'>
-      {/* <img
-        src='/pill.jpg'
-        className='block w-full h-[13rem] object-cover'
-        alt=''
-      /> */}
-
       <Image
-        src='/pill.jpg'
+        src={image}
         width='100%'
         height='55rem'
         layout='responsive'
@@ -19,15 +16,26 @@ export default function ProductCard({ details }) {
         alt=''
       />
       <div className='p-4 text-gray-900 grid grid-cols-1'>
-        <span className='font-medium'>Hydrocodone Pill</span>
+        <span className='font-medium'>{product}</span>
 
         <div className='flex justify-between items-center'>
           <span className='font-light'>
-            Price: <span className='text-red-800'>$20</span>
+            Price: <span className='text-red-800'>{price}</span>
           </span>
 
-          <span className='text-slate-800 hover:text-red-900 cursor-pointer transition-all delay-100 ease-out p-1'>
-            <FaCartPlus size={24} />
+          <span
+            className='text-slate-800 hover:text-red-900 cursor-pointer transition-all delay-100 ease-out p-1'
+            onClick={() => {
+              setAdded(!added);
+            }}
+          >
+            {added ? (
+              <span className='bg-yellow-500 hover:bg-yellow-400 rounded-md p-3 font-light text-gray-900'>
+                Remove from Cart
+              </span>
+            ) : (
+              <FaCartPlus size={24} />
+            )}
           </span>
         </div>
       </div>
