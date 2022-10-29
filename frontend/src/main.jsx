aa69e56d32ccf4f6b606ff0b3df515efff196a9f
omarageorge <georgeomara8@gmail.com>
2022-10-29 22:59:02 +0300

    Another commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import UserProvider from './context/userContext';
 import App from './App';
 import './styles/globals.scss';
 
-axios.defaults.baseURL = 'http://localhost:5000';
+// axios.defaults.baseURL = 'http://localhost:5000';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
