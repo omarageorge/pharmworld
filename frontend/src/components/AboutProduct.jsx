d45f425035b9f12d162ed25ef53f12b799e3bb69
omarageorge <georgeomara8@gmail.com>
2022-11-01 16:59:02 +0300

    Another commit

--- frontend/src/components/AboutProduct.jsx
+++ frontend/src/components/AboutProduct.jsx
@@ -1,11 +1,10 @@
 export default function AboutProduct({ name, description }) {
   return (
     <div className='flex flex-col'>
-      <span className='font-normal text-xl'>
-        {/* <strong>{name}</strong> */}
-        {name}
+      <span className='font-bold text-xl'>
+        <strong>{name}</strong>
       </span>
-      <span className='font-light text-lg'>{description}</span>
+      <span className='font-light text-xl'>{description}</span>
     </div>
   );
 }
