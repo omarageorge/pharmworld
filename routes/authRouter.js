612e32876f7f42de135ef06a87fafa5e5d20e6c6
Omara George <georgeomara8@gmail.com>
2023-04-30 10:06:40 +0300

    Anothert commit

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,37 +1,37 @@
-import { Router } from 'express';
-import passport from 'passport';
+import { Router } from "express";
+import passport from "passport";
 import {
   registerUser,
   logoutUser,
   redirectUser,
-} from '../controllers/userController.js';
+} from "../controllers/userController.js";
 
 const router = Router();
 
 // @route   GET /auth/redirect
 // @desc    Redirect a user to the appropriate page
 // @access  Public/Authenticated
-router.get('/redirect', redirectUser);
+router.get("/redirect", redirectUser);
 
 // @route   POST /auth/register
 // @desc    Register a new user
 // @access  Public
-router.post('/register', registerUser);
+router.post("/register", registerUser);
 
 // @route   POST /auth/login
 // @desc    Login a user
 // @access  Public
 router.post(
-  '/login',
-  passport.authenticate('local', {
-    successRedirect: '/auth/redirect',
-    failureRedirect: '/login',
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/auth/redirect",
+    failureRedirect: "/login",
   })
 );
 
 // @route   POST /auth/logout
 // @desc    Logout a user
 // @access  Private
-router.post('/logout', logoutUser);
+router.post("/logout", logoutUser);
 
 export default router;
